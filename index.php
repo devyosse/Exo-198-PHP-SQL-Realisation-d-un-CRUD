0e4b7598d9f41fca016c317170ddf7b24a79a760
devyosse <narutoshippuden884@gmail.com>
2022-02-23 14:26:11 +0100

    exo198

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require __DIR__ . "/function/student.php";
 $object = new Database();
 $start = $object->getInstance();
 
-//newStudent('Parker', 'Peter', 24, $start);
-//updateStudent('Olivier', 'De la paix', 100, 1, $start);
+//newStudent('Parker', 'Peter', 24, $start); OK
+//updateStudent('Olivier', 'De la paix', 100, 1, $start); OK
 
 deleteStudent(1, $start);
